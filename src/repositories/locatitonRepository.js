d7bd6d7c561d4e4aa9f30f3f111e6e67d7e60c85
trongdat <trongdat2322003@gmail.com>
2024-11-03 01:44:19 +0700

    fix shop and location

--- src/repositories/locatitonRepository.js
+++ src/repositories/locatitonRepository.js
@@ -1,79 +1,102 @@
-const locationModel = require('../models/locationModel')
-const {BadRequestError, NotFoundError} = require('../core/errorResponse')
-const {removeUndefinedObject} = require('../utils/index')
-const userModel = require('../models/userModel')
-const dotenv = require('dotenv')
+const locationModel = require("../models/locationModel");
+const { BadRequestError, NotFoundError } = require("../core/errorResponse");
+const {
+  removeUndefinedObject,
+  isDuplicateNameOnCreate,
+} = require("../utils/index");
+const userModel = require("../models/userModel");
+const dotenv = require("dotenv");
 
-dotenv.config()
-const createLocation = async ({payload, user})=>{
-    if(!user){
-        throw new BadRequestError('User not found')
+dotenv.config();
+const createLocation = async ({ payload, user }) => {
+  const { location_name } = payload;
+  if (!user) {
+    throw new BadRequestError("User not found");
+  }
+  const checkRoles = await userModel.findOne({
+    _id: user._id,
+    roles: process.env.ROLES_ADMIN,
+  });
+  if (!checkRoles) {
+    throw new BadRequestError(
+      "User does not have sufficient permissions to perform this action"
+    );
+  }
+  // Kiểm tra trùng lặp địa điểm
+  if (location_name) {
+    const checkName = await isDuplicateNameOnCreate({
+      model: locationModel,
+      fieldName: "location_name",
+      name: location_name,
+    });
+    if (checkName) {
+      throw new BadRequestError("Category name already exists");
     }
-    const checkRoles = await userModel.findOne({
-        _id: user._id,
-        roles: process.env.ROLES_ADMIN
+  }
+  const newLocation = await locationModel.create(payload);
+  if (!newLocation) {
+    throw new BadRequestError("Failed to create location");
+  }
+  return newLocation;
+};
+
+const getAllLocations = async () => {
+  const locations = await locationModel
+    .find({
+      isDeleted: false,
     })
-    if(!checkRoles){
-        throw new BadRequestError('User does not have sufficient permissions to perform this action')
-    }
-    const newLocation = await locationModel.create(payload)
-    if(!newLocation){
-        throw new BadRequestError('Failed to create location')
-    }
-    return newLocation
-}
+    .lean();
+  if (!locations) {
+    throw new NotFoundError("No locations found");
+  }
+  return locations;
+};
 
-const getAllLocations = async()=>{
-    const locations = await locationModel.find({
-        isDeleted: false
-    }).lean()
-    if(!locations){
-        throw new NotFoundError('No locations found')
-    }
-    return locations
-}
+const getLocationById = async ({ location_id }) => {
+  const location = await locationModel.findById(location_id).lean();
+  if (!location) throw new NotFoundError("Location not found");
+  return location;
+};
 
-const getLocationById = async (location_id) => {
-    const location = await locationModel.findById(location_id).lean()
-    if (!location) throw new NotFoundError('Location not found')
-    return location
-}
+const updateLocationById = async ({ location_id, payload, user }) => {
+  if (!user) {
+    throw new BadRequestError("User not found");
+  }
+  const checkRoles = await userModel.findOne({
+    _id: user._id,
+    roles: process.env.ROLES_ADMIN,
+  });
+  const cleanData = removeUndefinedObject(payload);
+  const updatedLocation = await locationModel
+    .findByIdAndUpdate(location_id, cleanData, { new: true })
+    .lean();
+  if (!updatedLocation) {
+    throw new NotFoundError("Location not found");
+  }
+  return updatedLocation;
+};
 
-const updateLocationById = async ({location_id, payload, user})=>{
-    if(!user){
-        throw new BadRequestError('User not found')
-    }
-    const checkRoles = await userModel.findOne({
-        _id: user._id,
-        roles: process.env.ROLES_ADMIN
-    })
-    const cleanData = removeUndefinedObject(payload)
-    const updatedLocation = await locationModel.findByIdAndUpdate(location_id, cleanData, {new: true}).lean()
-    if(!updatedLocation){
-        throw new NotFoundError('Location not found')
-    }
-    return updatedLocation
-}
-
-const deleteLocationById = async ({location_id, user}) => {
-    if(!user){
-        throw new BadRequestError('User not found')
-    }
-    const checkRoles = await userModel.findOne({
-        _id: user._id,
-        roles: process.env.ROLES_ADMIN
-    })
-    const updateLocation = await locationModel.findByIdAndUpdate(location_id, {isDeleted: true}).lean()
-    if(!updateLocation){
-        throw new NotFoundError('Location not found')
-    }
-    return updateLocation
-}
+const deleteLocationById = async ({ location_id, user }) => {
+  if (!user) {
+    throw new BadRequestError("User not found");
+  }
+  const checkRoles = await userModel.findOne({
+    _id: user._id,
+    roles: process.env.ROLES_ADMIN,
+  });
+  const updateLocation = await locationModel
+    .findByIdAndUpdate(location_id, { isDeleted: true })
+    .lean();
+  if (!updateLocation) {
+    throw new NotFoundError("Location not found");
+  }
+  return updateLocation;
+};
 
 module.exports = {
-    createLocation,
-    getAllLocations,
-    getLocationById,
-    updateLocationById,
-    deleteLocationById,
-}
+  createLocation,
+  getAllLocations,
+  getLocationById,
+  updateLocationById,
+  deleteLocationById,
+};
--- src/services/shopService.js
+++ src/services/shopService.js
@@ -1,42 +1,67 @@
-const shopModel = require('../models/shopModel')
-const {findByEmail, findById} = require('../repositories/userRepository')
-const {BadRequestError} = require('../core/errorResponse')
-const { getInfoData } = require('../utils')
-const {removeUndefinedObject} = require('../utils/index')
-class ShopService{
-    static createShop = async({user, shop_name, shop_location, shop_image})=>{
-        if(!user){
-            throw new BadRequestError('not found user')
-        }
-        const newShop = await shopModel.create({
-            shop_name,
-            shop_location,
-            shop_image,
-            shop_owner: user._id
-        })
-        return {
-            shop: getInfoData({fileds:['shop_name','shop_location','shop_image'],object: newShop})
-        }
+const shopModel = require("../models/shopModel");
+const { findByEmail, findById } = require("../repositories/userRepository");
+const { BadRequestError } = require("../core/errorResponse");
+const { getInfoData } = require("../utils");
+const { removeUndefinedObject } = require("../utils/index");
+class ShopService {
+  static createShop = async ({ user, shop_name, location_id, shop_image }) => {
+    if (!user) {
+      throw new BadRequestError("not found user");
     }
-    static updateShop = async({user, bodyUpdate})=>{
-        if(!user){
-            throw new BadRequestError('not found user')
-        }
-        const foundShop = await shopModel.findOne({shop_owner: user._id})
-        if(!foundShop){
-            throw new BadRequestError('not found shop')
-        }
-        const cleanedUpdate = removeUndefinedObject(bodyUpdate)
-        const updatedShop = await shopModel.findByIdAndUpdate(foundShop._id, cleanedUpdate,{
-            new: true,
-            runValidators: true
-        })
-        if(!updatedShop){
-            throw new BadRequestError('update shop fail')
-        }
-        return {
-            shop: getInfoData({fileds:['shop_name','shop_location','shop_image'],object: updatedShop})
-        }
+    // Kiểm tra xem tên shop đã tồn tại trong cùng khu vực chưa
+    const existingShop = await shopModel.findOne({
+      shop_name: shop_name,
+      location_id: location_id,
+    });
+
+    if (existingShop) {
+      throw new BadRequestError("Shop name already exists in this location");
     }
+    const newShop = await shopModel.create({
+      shop_name,
+      location_id,
+      shop_image,
+      shop_owner: user._id,
+    });
+    return {
+      shop: getInfoData({
+        fileds: ["shop_name", "location_id", "shop_image"],
+        object: newShop,
+      }),
+    };
+  };
+  static updateShop = async ({ shop_id, user, bodyUpdate }) => {
+    if (!user) {
+      throw new BadRequestError("not found user");
+    }
+    console.log(shop_id);
+
+    // Tìm shop theo ID, không sử dụng findOne
+    const foundShop = await shopModel.findById(shop_id);
+    if (!foundShop) {
+      throw new BadRequestError("not found shop");
+    }
+
+    const cleanedUpdate = removeUndefinedObject(bodyUpdate);
+
+    const updatedShop = await shopModel.findByIdAndUpdate(
+      shop_id,
+      cleanedUpdate,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!updatedShop) {
+      throw new BadRequestError("update shop fail");
+    }
+
+    return {
+      shop: getInfoData({
+        fileds: ["shop_name", "shop_location", "shop_image"],
+        object: updatedShop,
+      }),
+    };
+  };
 }
-module.exports = ShopService
+module.exports = ShopService;
