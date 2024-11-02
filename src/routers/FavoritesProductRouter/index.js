4b6a3d2176d6cfe95df1301c0c027701d6cdf4ee
Hoangnam133 <012003hoangnam@gmail.com>
2024-11-02 23:42:28 +0700

    favorite

--- src/routers/FavoritesProductRouter/index.js
+++ src/routers/FavoritesProductRouter/index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const {authentication} = require('../../auth/authUtils')
 const router = express.Router()
 
-const FavoritesProductController = require('../../controllers/FavoritesProductController')
+const FavoriteController = require('../../controllers/FavoriteController')
 
 const { asynHandler } = require('../../utils/handler')
 
 router.use(authentication)
 
-router.get('/getFavorites',asynHandler(FavoritesProductController.getFavorites))
-router.patch('/deleteProductInFavorites/:product_id',asynHandler(FavoritesProductController.deleteFavorite))
-router.patch('/toggleFavorite/:product_id',asynHandler(FavoritesProductController.toggleFavorite))
+router.get('/getFavorites',asynHandler(FavoriteController.getFavorites))
+router.post('/addFavorite/:product_id',asynHandler(FavoriteController.toggleFavorite))
+router.post('/deleteFavorite/:product_id',asynHandler(FavoriteController.deleteFavorite))
+
+
+
 module.exports = router
 // mỗi khi xóa sản phẩm ở ADMIN thì xóa luôn sản phẩm trong giỏ hàng và trong danh sách yêu thích
