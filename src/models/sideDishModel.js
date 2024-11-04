8128261994e75c6a7f288066560d4b2b14ced920
Hoangnam133 <012003hoangnam@gmail.com>
2024-11-05 00:27:32 +0700

    sideDish

--- src/models/sideDishModel.js
+++ src/models/sideDishModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const sideDishSchema = new Schema({
-    name:{
+    sideDish_name:{
         type: String,
         required: true, // Tên món phụ là bắt buộc
     },
@@ -10,13 +10,9 @@ const sideDishSchema = new Schema({
         type: Number,
         required: true, // Giá món phụ là bắt buộc
     },
-    description: {
-        type: String,
-        default: '' // Mô tả món phụ (tuỳ chọn)
-    },
-    isAvailable: {
+    isDeleted: {
         type: Boolean,
-        default: true, // Trạng thái có sẵn
+        default: true, 
     },
     sideDish_image: {
         type: String,
