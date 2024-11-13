2c7528e822ca05624768941cf3a542cf9b3cc504
trongdat <trongdat2322003@gmail.com>
2024-11-13 20:32:28 +0700

    merge main

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,129 +1,140 @@
-const { model, Schema } = require('mongoose');
-const { convertToVietnamTime } = require('../utils/convertTime');
-const DOCUMENT_NAME = 'Order';
-const COLLECTION_NAME = 'Orders';
-const ExtraSchema = new Schema({
-    sideDish_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'sideDish',  
-        required: true,
-    },
-    sideDish_name: {
-        type: String,
-        required: true,
-    },
-    quantity: {
-        type: Number,
-        required: true,
-        default: 1,
-    }
-})
-const checkOutSchema = new Schema({
-    totalAmount: {
-        type: Number,
-        default: 0,
-        required: true
-    },
-    totalDiscount: {
-        type: Number,
-        default: 0,
-    },
-    finalPrice: {
-        type: Number,
-        default: 0
-    }
-}, {
-    _id: false
-})
-const paymentSchema = new Schema({
-    payment_method: {
-        type: String,
-        enum: ['cash_payment', 'online_payment'],
-        required: true,
-        default: 'online_payment'
-    },
-    payment_status: {
-        type: String,
-        enum: ['pending', 'paid', 'failed', 'success'],
-        default: 'pending'
-    }
-}, { _id: false });
+const { model, Schema } = require("mongoose");
+const moment = require("moment-timezone");
 
-const orderSchema = new Schema({
-    order_userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    order_checkout: {
-        type: checkOutSchema,
-        required: true
-    },
-    order_payment: {
-        type: paymentSchema,
-        required: true
-    },
-    order_product: [{
-        _id: false,
-        product_id: {
-            type: Schema.Types.ObjectId,
-            ref: 'Product',
-            required: true
-        },
-        quantity: {
-            type: Number,
-            required: true
-        },
-        // price:{
-        //     type: Number,
-        //     required: true
-        // },
-        totalPrice: {
-            type: Number,
-            required: true
-        },
-        product_thumb:{
-            type: String,
-            required: true
-        },
-        product_name:{
-            type: String,
-            required: true
-        },
-        extra: [ExtraSchema]
-    }],
-    order_trackingNumber: {
-        type: String,
-        default: () => `#${Math.floor(1000000000 + Math.random() * 9000000000)}`
-    },
-    order_status: {
-        type: String,
-        enum: ['pending', 'completed', 'cancelled', 'success'],
-        default: 'pending'
-    },
-    order_time: {
-        type: Date,
-        required: true
-    },
-    options_delivery:{
-        type: String,
-        enum: ['asap', 'specific_time'],
-        default: 'asap'
-    },
-    estimated_delivery_time: {
-        type: Date,  // Thời gian giao hàng dự kiến =  order_time + thời gian làm món + thời gian đặt trước nếu có , sau đó sẽ đưa vô hàng đợi và tính toán lại
-        required: true
-    },
-    order_discount_code:{
-        type: String,
-        default: null
-    },
-    note:{
-        type: String,
-        default: null
-    }
-},{
-    timestamps: true,
-    collection: COLLECTION_NAME
+const DOCUMENT_NAME = "Order";
+const COLLECTION_NAME = "Orders";
+
+const ExtraSchema = new Schema({
+  sideDish_id: {
+    type: Schema.Types.ObjectId,
+    ref: "sideDish",
+    required: true,
+  },
+  sideDish_name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
 });
 
-module.exports = model(DOCUMENT_NAME, orderSchema)
+const checkOutSchema = new Schema(
+  {
+    totalAmount: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    totalDiscount: {
+      type: Number,
+      default: 0,
+    },
+    finalPrice: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    _id: false,
+  }
+);
+
+const paymentSchema = new Schema(
+  {
+    payment_method: {
+      type: String,
+      enum: ["cash_payment", "online_payment"],
+      required: true,
+      default: "online_payment",
+    },
+    payment_status: {
+      type: String,
+      enum: ["pending", "paid", "failed", "success"],
+      default: "pending",
+    },
+  },
+  { _id: false }
+);
+
+const orderSchema = new Schema(
+  {
+    order_userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    order_checkout: {
+      type: checkOutSchema,
+      required: true,
+    },
+    order_payment: {
+      type: paymentSchema,
+      required: true,
+    },
+    order_product: [
+      {
+        _id: false,
+        product_id: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        totalPrice: {
+          type: Number,
+          required: true,
+        },
+        product_thumb: {
+          type: String,
+          required: true,
+        },
+        product_name: {
+          type: String,
+          required: true,
+        },
+        extra: [ExtraSchema],
+      },
+    ],
+    order_trackingNumber: {
+      type: String,
+      default: () => `#${Math.floor(1000000000 + Math.random() * 9000000000)}`,
+    },
+    order_status: {
+      type: String,
+      enum: ["pending", "completed", "cancelled", "success"],
+      default: "pending",
+    },
+    order_time: {
+      type: String, // Thay vì Date, sử dụng String để lưu chuỗi ISO
+      required: true,
+    },
+    options_delivery: {
+      type: String,
+      enum: ["asap", "specific_time"],
+      default: "asap",
+    },
+    estimated_delivery_time: {
+      type: String, // Thay vì Date, sử dụng String để lưu chuỗi ISO
+      required: true,
+    },
+    order_discount_code: {
+      type: String,
+      default: null,
+    },
+    note: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+module.exports = model(DOCUMENT_NAME, orderSchema);
--- src/routers/cartRouterV2/index.js
+++ src/routers/cartRouterV2/index.js
@@ -1,15 +1,24 @@
-const express = require('express')
-const {authentication, authorizeRoles} = require('../../auth/authUtils')
-const router = express.Router()
+const express = require("express");
+const { authentication, authorizeRoles } = require("../../auth/authUtils");
+const router = express.Router();
 
-const cartv2Controller = require('../../controllers/cartv2Controller')
-const { asynHandler } = require('../../utils/handler')
+const cartv2Controller = require("../../controllers/cartv2Controller");
+const { asynHandler } = require("../../utils/handler");
 
-router.use(authentication)
-router.post('/addToCart', asynHandler(cartv2Controller.addToCart))
-router.post('/deleteProductInCart', asynHandler(cartv2Controller.deleteProductInCart))
-router.patch('/incProductQuantity', asynHandler(cartv2Controller.incProductQuantity))
-router.patch('/decProductQuantity', asynHandler(cartv2Controller.DecProductQuantity))
-router.get('/getCartByUserId', asynHandler(cartv2Controller.getCartByUserId))
+router.use(authentication);
+router.post("/addToCart", asynHandler(cartv2Controller.addToCart));
+router.post(
+  "/deleteProductInCart",
+  asynHandler(cartv2Controller.deleteProductInCart)
+);
+router.patch(
+  "/incProductQuantity",
+  asynHandler(cartv2Controller.incProductQuantity)
+);
+router.patch(
+  "/decProductQuantity",
+  asynHandler(cartv2Controller.DecProductQuantity)
+);
+router.get("/getCartByUserId", asynHandler(cartv2Controller.getCartByUserId));
 
-module.exports = router
+module.exports = router;
