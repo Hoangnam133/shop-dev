fabba77696022b01a804be7ceb1feb4d927477ca
Hoangnam133 <012003hoangnam@gmail.com>
2024-12-01 00:08:43 +0700

    fix-or

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const {app:{port}} = require('./src/configs/configMongodb')
 const server = app.listen(port, ()=>{
     console.log(`server started port ${port}`)
 })
+
 process.on('SIGINT', async () => {
     console.log('Received SIGINT, shutting down gracefully...');
+    await syncAllCartsToDatabase();  // Đồng bộ giỏ hàng vào MongoDB trước khi tắt
+    server.close(() => {
+        console.log('Server closed');
+        process.exit(0); // Dừng tiến trình sau khi server đóng
+    });
+});
+
+process.on('SIGTERM', async () => {
+    console.log('Received SIGTERM, shutting down gracefully...');
     await syncAllCartsToDatabase();  // Đồng bộ giỏ hàng vào MongoDB trước khi tắt
     server.close(() => {
         console.log('Server closed');
